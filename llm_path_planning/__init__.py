6780d776f981c12de7ac847351f3e1b3e16b209d
Jay <j456ay@gmail.com>
2025-09-05 14:17:12 +0900

    Update __init__.py

--- llm_path_planning/__init__.py
+++ llm_path_planning/__init__.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""
+LLM Path Planning Package
+TurtleBot3 + ROS2 Humble 환경에서 LLM을 활용한 경로 계획 패키지
+"""
 
+__version__ = '1.0.0'
+__author__ = 'Your Name'
+__email__ = 'your_email@example.com'
